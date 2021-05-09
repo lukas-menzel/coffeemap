5742af13808c58e0240915d4b18e4f9ce3d542d4
Lukas Menzel <lukas.menzel@code.berlin>
2021-05-09 23:35:55 +0200

    fix 16

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ import flask_cors
 from flask_migrate import Migrate
 
 
-# Initialize flask app for the example
+# Initialize flask app
 app = flask.Flask(__name__, static_folder='./build', static_url_path='/')
 app.debug = True
 app.config['SECRET_KEY'] = '254wsztdrhfjcghvkjlkio8i7ttgh'
 app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
-
-db = flask_sqlalchemy.SQLAlchemy()
+db = flask_sqlalchemy.SQLAlchemy(app)
 guard = flask_praetorian.Praetorian()
 cors = flask_cors.CORS()
 migrate = Migrate(app, db)
 
 
-# Initialize a local database for the example
+# Initialize a database
 if os.environ.get('DATABASE_URL'):
   app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 else:
@@ -28,14 +27,12 @@ db.init_app(app)
 
 from models import User
 from routes import MyJSONEncoder
-# Initialize the flask-praetorian instance for the app
+# Initialize the flask-praetorian instance
 guard.init_app(app, User)
 app.json_encoder = MyJSONEncoder
 
 
-# Initializes CORS so that the api_tool can talk to the example app
 cors.init_app(app)
 
-# Run the example
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
