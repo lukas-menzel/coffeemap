d8d24bc270639f6b842e412c12b028a291eb9d23
Lukas Menzel <lukas.menzel@code.berlin>
2021-05-10 22:56:41 +0200

    small fix single café

--- src/components/SinglePlace.js
+++ src/components/SinglePlace.js
@@ -45,18 +45,18 @@ const SinglePlace = ({ match }) => {
             <div class="w-full overflow-hidden md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
             <div className="h-48 cursor-pointer p-12 text-center bg-white rounded-md shadow">
             <h2 className="text-xl font-bold">Links:</h2>
-            <a href="{places.place.website_url}">Website</a><br />
-            <a href="{places.place.facebook_url}">Facebook</a><br />
-            <a href="{places.place.instagram_url}">Instagram</a><br /></div>
+            <a href={places.place.website_url}>Website</a><br />
+            <a href={places.place.facebook_url}>Facebook</a><br />
+            <a href={places.place.instagram_url}>Instagram</a><br /></div>
             </div>
             </div>
             <div class="flex flex-wrap overflow-hidden md:-mx-2 lg:-mx-2 xl:-mx-2">
             <div class="w-full overflow-hidden md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
             <div className="h-48 cursor-pointer p-12 text-center bg-white rounded-md shadow">
               <h2 className="text-xl font-bold">Prices:</h2>
-              Espresso: {places.place.price_espresso}<br />
-              Filter coffee: {places.place.price_filter_coffee}<br />
-              Cappuccino: {places.place.price_cappuccino}</div>
+              Espresso: {places.place.price_espresso}€<br />
+              Filter coffee: {places.place.price_filter_coffee}€<br />
+              Cappuccino: {places.place.price_cappuccino}€</div>
             </div>
 
             <div class="w-full overflow-hidden md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
